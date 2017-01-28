9c9850cc07581e65f5e92f6c1865565aa4c77f4d
Dominic Serrano <dmsrojo@gmail.com>
2017-01-28 00:36:34 -0600

    Added a timestamp to notes being patched. Need to add two buttons to save and stay on note view and one to save and exit

--- js/noteState.js
+++ js/noteState.js
@@ -18,35 +18,14 @@ const displaySelNote = (noteId) => {
   return fetch(`${URL}/notes/${noteId}.json`)
   .then(response => response.json())
   .then(noteObj => {
+    // Show note view screen
     getEl('.note-list').innerHTML = "";
     getEl('.note-text-div').classList.remove('hidden');
     getEl('.note-text-div textarea').value = noteObj.text;
     getEl('.note-title').value = noteObj.title;
+    getEl('.time-p').innerHTML = 'Last saved: ' + `<b>${noteObj.timeStamp}</b>`;
 
-    const b = document.createElement('input');
-    b.setAttribute('id', noteId);
-    b.setAttribute('class', 'save-button');
-    b.setAttribute('type', 'button');
-    b.setAttribute('value', 'Save Note');
-    getEl('.note-text-div').append(b);
-    b.addEventListener('click', (e) => {
-      fetch(`${URL}/notes/${e.target.id}.json`, {
-        method: 'PATCH',
-        body: JSON.stringify({
-          title: getEl('.note-title').value,
-          text: getEl('.note-text-div textarea').value,
-          inUse: false
-        })
-      })
-      .then(() => {
-        // Reset the note text div
-        getEl('.note-title').value = "";
-        getEl('.note-text-div textarea').value = "";
-        getEl('.note-text-div').classList.add('hidden');
-        getEl('.save-button').remove();
-      })
-      .then(() => getAllNotes())
-    });
+    createItemSaveButton(noteId);
 
     return noteId;
   })
@@ -58,4 +37,36 @@ const activateNoteInUse = (noteId) => {
     method: 'PATCH',
     body: JSON.stringify({ inUse: true })
   })
+};
+
+const createItemSaveButton = (noteId) => {
+  const b = document.createElement('input');
+  b.setAttribute('id', noteId);
+  b.setAttribute('class', 'save-button');
+  b.setAttribute('type', 'button');
+  b.setAttribute('value', 'Save Note');
+  getEl('.note-text-div').append(b);
+  b.addEventListener('click', saveCurrentNote);
+};
+
+// Will patch the edited note on firebase
+const saveCurrentNote = (e) => {
+  fetch(`${URL}/notes/${e.target.id}.json`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      title: getEl('.note-title').value,
+      text: getEl('.note-text-div textarea').value,
+      inUse: false,
+      timeStamp: `${Date().slice(4,10)} ${Date().slice(16, 21)}`
+    })
+  })
+  .then(() => {
+    // Reset the note text div
+    getEl('.note-title').value = "";
+    getEl('.note-text-div textarea').value = "";
+    getEl('.note-text-div').classList.add('hidden');
+    getEl('.save-button').remove();
+  })
+  .then(() => getAllNotes())
+  .catch(console.error);
 };
